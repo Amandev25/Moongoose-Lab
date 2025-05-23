18fe2876100c6fc23f48db408787de1f39dae6fb
Amandev25 <amanyadav.dev25@gmail.com>
2025-05-23 11:22:18 +0530

    Creating and Inserting our Data Into a Db

--- 1.Connections/index.js
+++ 1.Connections/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import connectDB from './db/connectDB.js';
-import movie from './models/movies.js';
+import { createDoc } from './models/movies.js';
 const app = express();
 const port = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL|| 'mongodb://127.0.0.1:27017/movies'
+console.clear();
 connectDB(DATABASE_URL)
+createDoc();
 app.listen (port, ()=> console.log(`Server Listening on port ${port}`)
 );
 
--- 1.Connections/models/movies.js
+++ 1.Connections/models/movies.js
@@ -12,6 +12,56 @@ const movieSchema = new mongoose.Schema({
     isActive : {type:Boolean},
     Comments: [{value:{type:String} , published: {type:Date , default:Date.now}}]
 })
-// defining the mnodel
+// defining the model
 const movie  = mongoose.model("Movie" ,movieSchema);
-export default movie;
+ // Creating a document
+  const createDoc = async() => {
+    try {
+        // Cfeating new Document
+        const m1 = new movie({
+            name : "Avengers Endgame",
+            ratings: 4,
+            money:600000,
+            genre: ['action' , 'Sci-FI'],
+            isActive: true,
+            Comments: [{value : "That was an amazing movie!!"}]
+
+        })
+        const m2 = new movie({
+            name : "Avengers Infinity War",
+            ratings: 4.9,
+            money:900000,
+            genre: ['action' , 'Sci-FI'],
+            isActive: true,
+            Comments: [{value : "That was an amazing movie!!"}]
+
+        })
+        const m3 = new movie({
+            name : "Avengers Civil War",
+            ratings: 3.8,
+            money:890000,
+            genre: ['action' , 'Sci-FI'],
+            isActive: true,
+            Comments: [{value : "That was an amazing movie!!"}]
+
+        })
+        const m4 = new movie({
+            name : "Avengers : Age of Ultron",
+            ratings: 5,
+            money:560000,
+            genre: ['action' , 'Sci-FI'],
+            isActive: true,
+            Comments: [{value : "That was an amazing movie!!"}]
+
+        })
+        // Saving the document
+        const result = await movie.insertMany([m1,m2,m3,m4]);
+        console.log(result);
+    }catch(error)
+    {
+        console.log("error");
+        
+    }
+  }
+
+export { createDoc };
